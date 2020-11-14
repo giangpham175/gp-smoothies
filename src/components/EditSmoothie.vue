392ef0eef5cdc4b24314d5e0efa025e245f9d6cd
Giang <14110040@student.hcmute.edu.vn>
2020-11-15 00:49:36 +0700

    get data with smoothie_slug

--- src/components/EditSmoothie.vue
+++ src/components/EditSmoothie.vue
@@ -1,17 +1,30 @@
 <template>
-    <div class="edit-smoothie container">
-        <h2>Edit Smoothie {{ this.$route.params.smoothie_slug }}</h2>
+    <div class="edit-smoothie container" v-if="smoothie">
+        <h2>Edit {{ smoothie.title }} Smoothie</h2>
+        {{smoothie}}
     </div>
 </template>
 
 <script>
+import db from '@/firebase/init.js'
+
 export default {
     name: 'EditSmoothie',
     
     data() {
         return {
-
+            smoothie: null
         }
+    }, 
+
+    created() {
+        let ref = db.collection('smoothies').where('slug', '==', this.$route.params.smoothie_slug)
+        ref.get().then(snapshot => {
+            snapshot.forEach(doc => {
+                this.smoothie = doc.data()
+                this.smoothie.id = doc.id
+            })
+        })
     }
 }
 </script>
