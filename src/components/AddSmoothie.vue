d1928f81426ecdbd1cf37d1b7781120e48a4682b
Giang <14110040@student.hcmute.edu.vn>
2020-11-14 23:44:50 +0700

    delete ingredient

--- src/components/AddSmoothie.vue
+++ src/components/AddSmoothie.vue
@@ -8,9 +8,10 @@
                 <input type="text" name="title" v-model="title">
             </div>
 
-            <div v-for="(ing, i) in ingredients" :key="i">
+            <div class="field" v-for="(ing, i) in ingredients" :key="i">
                 <label for="ingredient">Ingredient:</label>
                 <input type="text" name="ingredient" v-model="ingredients[i]">
+                <i class="material-icons delete" @click="deleteIng(ing)">delete</i>
             </div>
 
             <div class="field add-ingredient">
@@ -73,6 +74,7 @@ export default {
                 this.feedback = 'You must enter a smoothie title'
             }
         },
+
         addIng() {
             if(this.another) {
                 this.ingredients.push(this.another)
@@ -83,6 +85,12 @@ export default {
             } else {
                 this.feedback = 'You must enter a value to add an ingredient'
             }
+        },
+
+        deleteIng(ing) {
+            this.ingredients = this.ingredients.filter(ingredient => {
+                return ingredient != ing
+            })
         }
     }
 }
@@ -102,5 +110,16 @@ export default {
 
 .add-smoothie .field {
     margin: 20px auto;
+    position: relative;
 }
+
+.add-smoothie .delete {
+    position: absolute;
+    right: 0;
+    bottom: 16px;
+    color: #aaa;
+    font-size: 1.4em;
+    cursor: pointer;
+}
+
 </style>
