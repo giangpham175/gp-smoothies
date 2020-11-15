ace462677914cf9605cc8fca659073bc70a4b3f9
Giang <14110040@student.hcmute.edu.vn>
2020-11-15 13:28:58 +0700

    deploy to firebase

--- src/components/EditSmoothie.vue
+++ src/components/EditSmoothie.vue
@@ -29,6 +29,7 @@
 
 <script>
 import db from '@/firebase/init.js'
+import slugify from 'slugify'
 
 export default {
     name: 'EditSmoothie',
@@ -43,7 +44,33 @@ export default {
 
     methods: {
         EditSmoothie() {
-            console.log(this.smoothie.title, this.smoothie.ingredients);
+            // console.log(this.smoothie.title, this.smoothie.ingredients);
+
+            if(this.smoothie.title) {
+                this.feedback = null
+                
+                // create a slug
+                this.smoothie.slug = slugify(this.smoothie.title, {
+                    replacement: '-',
+                    remove: /[$*_+~.()'"!\-:@]/g,
+                    lower: true
+                })
+
+                db.collection('smoothies').doc(this.smoothie.id)
+                .update({
+                    title: this.smoothie.title,
+                    ingredients: this.smoothie.ingredients,
+                    slug: this.smoothie.slug
+                })
+                .then(() => {
+                    this.$router.push('/')
+                })
+                .catch(err => {
+                    console.log(err);
+                })
+            } else {
+                this.feedback = 'You must enter a smoothie title'
+            }
         },
 
         addIng() {
